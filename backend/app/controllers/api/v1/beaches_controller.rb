2a18cac5bcf992b3b25d802e33091628c51ab309
brad-bartlett <bbartlett222@gmail.com>
2020-05-21 13:13:12 +1000

    controllers

--- backend/app/controllers/api/v1/beaches_controller.rb
+++ backend/app/controllers/api/v1/beaches_controller.rb
@@ -1,3 +1,13 @@
 class Api::v1:BeachesController < ApplicationController
 
+    def index
+        @beaches = Beach.all
+        render json: @beaches, status: 200
+    end
+    
+    def show
+        @beach = Beach.find(params[:id])
+        render json: @beach, status: 200
+    end
+
 end
--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,3 +1,24 @@
 class Api::v1:SessionsController < ApplicationController
 
+    def index
+        @sessions = Session.all
+        render json: @sessions, status: 200
+    end
+
+    def create
+        @session = Session.create(params[:session]]
+        render json: @session, status: 200
+    end
+
+
+    def show
+        @session = Session.find(params[:id])
+        render json: @session, status: 200
+    end
+
+    def destroy   
+        @session = Session.find(params[:id])
+        @session.destroy!
+        render json: @sessions, status: 200
+    end
 end
