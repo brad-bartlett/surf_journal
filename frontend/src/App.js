3bf7125f43423b7900f92c401a6beb8dc61f1cc3
brad-bartlett <bbartlett222@gmail.com>
2020-05-29 16:59:22 +1000

    fixed form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css';
 import Navbar from './layout/Navbar';
 import SessionForm from './components/SessionForm'
 
 
-class App extends React.Component() {
+class App extends Component {
   
   constructor() {
     super()
@@ -12,30 +12,15 @@ class App extends React.Component() {
         datetime: `${new Date()}`,
         board: '',
         description: ''}
-
-      
   }
     
-
-    handleSubmit(e) {
-        alert('Submitted:' + this.state.value);
-        e.preventDefault()
-    }
-
-    handleChange(e) {
-        this.setState({
-            datetime: e.target.datetime,
-            board: e.target.board,
-            description: e.target.description
-        })
-    }
 
 
   render() {
     return (
     <div className="App">
       <Navbar />
-      <SessionForm handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
+      <SessionForm datetime={this.state.datetime} board={this.state.board} description={this.state.description}/>
     </div> 
   );
 }}
--- frontend/src/components/SessionForm.js
+++ frontend/src/components/SessionForm.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react'
 
+
+
+
 export default class SessionForm extends Component {
     
+    handleSubmit(e) {
+        alert('Submitted:' + this.state.value);
+        e.preventDefault()
+    }
+
+    handleChange(e) {
+        this.setState({
+            datetime: e.target.datetime,
+            board: e.target.board,
+            description: e.target.description
+        })
+    }
     
     render(props) {
         return (
             <div>
-                <form onSubmit={props.handleSubmit}>
+                <form onSubmit={this.handleSubmit}>
                     <label>
                         Date:
-                        <input type="datetime-local" value={props.state.datetime} onChange={props.handleChange} />
+                        <input type="datetime-local" value={this.props.datetime} onChange={this.handleChange} />
                     </label> <br/>
 
                     <label>
                         Board:
-                        <input type="text" value={props.state.board} onChange={props.handleChange} />
+                        <input type="text" value={this.props.board} onChange={this.handleChange} />
                     </label><br/>
 
                     <label>
                         Description:
-                        <input type="text" value={props.state.description} onChange={props.handleChange} />
+                        <input type="text" value={this.props.description} onChange={this.handleChange} />
                     </label><br/>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
